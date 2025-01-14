51244111b78044e0b6f38b5f86bc9a5518e12a7e
prathamesh16013 <prathameshpatil16013@gmail.com>
2025-01-14 11:21:01 +0000

    second commit

--- myfile.java
+++ myfile.java
@@ -1,7 +1,7 @@
 public class myfile{
     public static void main(String[] args) {
         // Print "Hello, World!" to the console
-        System.out.println("Hello, World!");
+        System.out.println("Hello,my name is  World!");
     }
 }
 
