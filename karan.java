2a518b2177a7b52f402d8450a9d9c6e291bdf2a8
prathamesh16013 <prathameshpatil16013@gmail.com>
2025-01-15 09:51:53 +0000

    secondcommit

--- karan.java
+++ karan.java
@@ -1,6 +1,6 @@
 public class karan{
     public static void main(String[] args) {
         // Print "Hello, World!" to the console
-        System.out.println("hii second jenkinsHello,my name is  World!");
+        System.out.println("okk change made first automationhii second jenkinsHello,my name is  World!");
     }
 }
